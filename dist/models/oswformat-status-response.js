1b158a0424a108a18830db5a7b463aa039a8b24b
workflow item <workflow_dummy@gaussian.com>
2024-01-05 12:09:13 +0000

    system:auto generated API client

---
+++ dist/models/oswformat-status-response.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OSWFormatStatusResponseStatusEnum = void 0;
+/**
+    * @export
+    * @enum {string}
+    */
+var OSWFormatStatusResponseStatusEnum;
+(function (OSWFormatStatusResponseStatusEnum) {
+    OSWFormatStatusResponseStatusEnum["Started"] = "started";
+    OSWFormatStatusResponseStatusEnum["Completed"] = "completed";
+    OSWFormatStatusResponseStatusEnum["Failed"] = "failed";
+})(OSWFormatStatusResponseStatusEnum = exports.OSWFormatStatusResponseStatusEnum || (exports.OSWFormatStatusResponseStatusEnum = {}));
---
+++ dist/models/validation-status.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ValidationStatusStatusEnum = void 0;
+/**
+    * @export
+    * @enum {string}
+    */
+var ValidationStatusStatusEnum;
+(function (ValidationStatusStatusEnum) {
+    ValidationStatusStatusEnum["InProgress"] = "in-progress";
+    ValidationStatusStatusEnum["Completed"] = "completed";
+})(ValidationStatusStatusEnum = exports.ValidationStatusStatusEnum || (exports.ValidationStatusStatusEnum = {}));
