f4e957633d9453c3f9f497498b66342a07813996
workflow item <workflow_dummy@gaussian.com>
2024-02-28 09:15:57 +0000

    system:auto generated API client

---
+++ dist/models/dataset-bbox-status.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DatasetBboxStatusStatusEnum = void 0;
+/**
+    * @export
+    * @enum {string}
+    */
+var DatasetBboxStatusStatusEnum;
+(function (DatasetBboxStatusStatusEnum) {
+    DatasetBboxStatusStatusEnum["INPROGRESS"] = "IN-PROGRESS";
+    DatasetBboxStatusStatusEnum["COMPLETED"] = "COMPLETED";
+})(DatasetBboxStatusStatusEnum = exports.DatasetBboxStatusStatusEnum || (exports.DatasetBboxStatusStatusEnum = {}));
