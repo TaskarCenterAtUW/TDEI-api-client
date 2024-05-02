476a6f0523799d507a03718717fa0ab8969e37f1
workflow item <workflow_dummy@gaussian.com>
2024-05-02 11:38:23 +0000

    system:auto generated API client

--- dist/models/index.js
+++ dist/models/index.js
@@ -14,6 +14,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./confidence-tdei-dataset-id-body"), exports);
 __exportStar(require("./dataset-item"), exports);
 __exportStar(require("./feature"), exports);
 __exportStar(require("./geo-json-object"), exports);
