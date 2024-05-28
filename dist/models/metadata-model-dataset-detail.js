741cbcc9fb87b4030d2caefbcb61d4c9c07241dc
workflow item <workflow_dummy@gaussian.com>
2024-05-28 03:55:11 +0000

    system:auto generated API client

---
+++ dist/models/metadata-model-dataset-detail.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MetadataModelDatasetDetailDataSourceEnum = exports.MetadataModelDatasetDetailCollectionMethodEnum = void 0;
+/**
+    * @export
+    * @enum {string}
+    */
+var MetadataModelDatasetDetailCollectionMethodEnum;
+(function (MetadataModelDatasetDetailCollectionMethodEnum) {
+    MetadataModelDatasetDetailCollectionMethodEnum["Manual"] = "manual";
+    MetadataModelDatasetDetailCollectionMethodEnum["Transform"] = "transform";
+    MetadataModelDatasetDetailCollectionMethodEnum["Generated"] = "generated";
+    MetadataModelDatasetDetailCollectionMethodEnum["Other"] = "other";
+})(MetadataModelDatasetDetailCollectionMethodEnum = exports.MetadataModelDatasetDetailCollectionMethodEnum || (exports.MetadataModelDatasetDetailCollectionMethodEnum = {}));
+/**
+    * @export
+    * @enum {string}
+    */
+var MetadataModelDatasetDetailDataSourceEnum;
+(function (MetadataModelDatasetDetailDataSourceEnum) {
+    MetadataModelDatasetDetailDataSourceEnum["_3rdParty"] = "3rdParty";
+    MetadataModelDatasetDetailDataSourceEnum["TDEITools"] = "TDEITools";
+    MetadataModelDatasetDetailDataSourceEnum["InHouse"] = "InHouse";
+})(MetadataModelDatasetDetailDataSourceEnum = exports.MetadataModelDatasetDetailDataSourceEnum || (exports.MetadataModelDatasetDetailDataSourceEnum = {}));
