3479dabad5d8b7ecb0c5c64a5ced0870672169db
workflow item <workflow_dummy@gaussian.com>
2024-09-23 08:48:49 +0000

    system:auto generated API client

--- apis/authentication-api.ts
+++ apis/authentication-api.ts
@@ -85,6 +85,66 @@ export const AuthenticationApiAxiosParamCreator = function (configuration?: Conf
             };
         },
         /**
+         * Request for password recovery, sends an email with a link to reset the password.
+         * @summary Request for password recovery
+         * @param {string} body 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        recoverPassword: async (body: string, options: AxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'body' is not null or undefined
+            if (body === null || body === undefined) {
+                throw new RequiredError('body','Required parameter body was null or undefined when calling recoverPassword.');
+            }
+            const localVarPath = `/api/v1/recover-password`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, 'https://example.com');
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+            const localVarRequestOptions :AxiosRequestConfig = { method: 'POST', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication ApiKey required
+            if (configuration && configuration.apiKey) {
+                const localVarApiKeyValue = typeof configuration.apiKey === 'function'
+                    ? await configuration.apiKey("x-api-key")
+                    : await configuration.apiKey;
+                localVarHeaderParameter["x-api-key"] = localVarApiKeyValue;
+            }
+
+            // authentication AuthorizationToken required
+            // http bearer authentication required
+            if (configuration && configuration.accessToken) {
+                const accessToken = typeof configuration.accessToken === 'function'
+                    ? await configuration.accessToken()
+                    : await configuration.accessToken;
+                localVarHeaderParameter["Authorization"] = "Bearer " + accessToken;
+            }
+
+            localVarHeaderParameter['Content-Type'] = 'text/plain';
+
+            const query = new URLSearchParams(localVarUrlObj.search);
+            for (const key in localVarQueryParameter) {
+                query.set(key, localVarQueryParameter[key]);
+            }
+            for (const key in options.params) {
+                query.set(key, options.params[key]);
+            }
+            localVarUrlObj.search = (new URLSearchParams(query)).toString();
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            const needsSerialization = (typeof body !== "string") || localVarRequestOptions.headers['Content-Type'] === 'application/json';
+            localVarRequestOptions.data =  needsSerialization ? JSON.stringify(body !== undefined ? body : {}) : (body || "");
+
+            return {
+                url: localVarUrlObj.pathname + localVarUrlObj.search + localVarUrlObj.hash,
+                options: localVarRequestOptions,
+            };
+        },
+        /**
          * Re-issues access token provided the valid refresh token
          * @summary Re-issue access token
          * @param {string} body 
@@ -168,6 +228,20 @@ export const AuthenticationApiFp = function(configuration?: Configuration) {
             };
         },
         /**
+         * Request for password recovery, sends an email with a link to reset the password.
+         * @summary Request for password recovery
+         * @param {string} body 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async recoverPassword(body: string, options?: AxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => Promise<AxiosResponse<void>>> {
+            const localVarAxiosArgs = await AuthenticationApiAxiosParamCreator(configuration).recoverPassword(body, options);
+            return (axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
+                const axiosRequestArgs :AxiosRequestConfig = {...localVarAxiosArgs.options, url: basePath + localVarAxiosArgs.url};
+                return axios.request(axiosRequestArgs);
+            };
+        },
+        /**
          * Re-issues access token provided the valid refresh token
          * @summary Re-issue access token
          * @param {string} body 
@@ -201,6 +275,16 @@ export const AuthenticationApiFactory = function (configuration?: Configuration,
             return AuthenticationApiFp(configuration).authenticate(body, options).then((request) => request(axios, basePath));
         },
         /**
+         * Request for password recovery, sends an email with a link to reset the password.
+         * @summary Request for password recovery
+         * @param {string} body 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async recoverPassword(body: string, options?: AxiosRequestConfig): Promise<AxiosResponse<void>> {
+            return AuthenticationApiFp(configuration).recoverPassword(body, options).then((request) => request(axios, basePath));
+        },
+        /**
          * Re-issues access token provided the valid refresh token
          * @summary Re-issue access token
          * @param {string} body 
@@ -230,6 +314,17 @@ export class AuthenticationApi extends BaseAPI {
      */
     public async authenticate(body: LoginModel, options?: AxiosRequestConfig) : Promise<AxiosResponse<TokenResponse>> {
         return AuthenticationApiFp(this.configuration).authenticate(body, options).then((request) => request(this.axios, this.basePath));
+    }
+    /**
+     * Request for password recovery, sends an email with a link to reset the password.
+     * @summary Request for password recovery
+     * @param {string} body 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof AuthenticationApi
+     */
+    public async recoverPassword(body: string, options?: AxiosRequestConfig) : Promise<AxiosResponse<void>> {
+        return AuthenticationApiFp(this.configuration).recoverPassword(body, options).then((request) => request(this.axios, this.basePath));
     }
     /**
      * Re-issues access token provided the valid refresh token
