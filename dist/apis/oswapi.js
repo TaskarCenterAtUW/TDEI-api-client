dc2bfcf7b375cfb81d4b5f8cbddf9e17051cdb2b
workflow item <workflow_dummy@gaussian.com>
2024-09-25 14:23:30 +0000

    system:auto generated API client

--- dist/apis/oswapi.js
+++ dist/apis/oswapi.js
@@ -181,6 +181,66 @@ var OSWApiAxiosParamCreator = function (configuration) {
             });
         },
         /**
+         * Adds the \"incline\" tag to a specified dataset identified by the tdei_dataset_id. It takes the dataset ID as a parameter, processes the dataset to calculate the incline information, and updates the dataset with the incline tag. Returns the job_id for the incline calculation request.
+         * @summary Adds the incline tag to the dataset
+         * @param {string} tdei_dataset_id tdei_dataset_id for a dataset, represented as a uuid
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        datasetTagIncline: function (tdei_dataset_id, options) {
+            if (options === void 0) { options = {}; }
+            return __awaiter(_this, void 0, void 0, function () {
+                var localVarPath, localVarUrlObj, baseOptions, localVarRequestOptions, localVarHeaderParameter, localVarQueryParameter, accessToken, _a, query, key, key, headersFromBaseOptions;
+                return __generator(this, function (_b) {
+                    switch (_b.label) {
+                        case 0:
+                            // verify required parameter 'tdei_dataset_id' is not null or undefined
+                            if (tdei_dataset_id === null || tdei_dataset_id === undefined) {
+                                throw new base_1.RequiredError('tdei_dataset_id', 'Required parameter tdei_dataset_id was null or undefined when calling datasetTagIncline.');
+                            }
+                            localVarPath = "/api/v1/osw/dataset-inclination/{tdei_dataset_id}"
+                                .replace("{".concat("tdei_dataset_id", "}"), encodeURIComponent(String(tdei_dataset_id)));
+                            localVarUrlObj = new URL(localVarPath, 'https://example.com');
+                            if (configuration) {
+                                baseOptions = configuration.baseOptions;
+                            }
+                            localVarRequestOptions = __assign(__assign({ method: 'POST' }, baseOptions), options);
+                            localVarHeaderParameter = {};
+                            localVarQueryParameter = {};
+                            if (!(configuration && configuration.accessToken)) return [3 /*break*/, 5];
+                            if (!(typeof configuration.accessToken === 'function')) return [3 /*break*/, 2];
+                            return [4 /*yield*/, configuration.accessToken()];
+                        case 1:
+                            _a = _b.sent();
+                            return [3 /*break*/, 4];
+                        case 2: return [4 /*yield*/, configuration.accessToken];
+                        case 3:
+                            _a = _b.sent();
+                            _b.label = 4;
+                        case 4:
+                            accessToken = _a;
+                            localVarHeaderParameter["Authorization"] = "Bearer " + accessToken;
+                            _b.label = 5;
+                        case 5:
+                            query = new URLSearchParams(localVarUrlObj.search);
+                            for (key in localVarQueryParameter) {
+                                query.set(key, localVarQueryParameter[key]);
+                            }
+                            for (key in options.params) {
+                                query.set(key, options.params[key]);
+                            }
+                            localVarUrlObj.search = (new URLSearchParams(query)).toString();
+                            headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+                            localVarRequestOptions.headers = __assign(__assign(__assign({}, localVarHeaderParameter), headersFromBaseOptions), options.headers);
+                            return [2 /*return*/, {
+                                    url: localVarUrlObj.pathname + localVarUrlObj.search + localVarUrlObj.hash,
+                                    options: localVarRequestOptions,
+                                }];
+                    }
+                });
+            });
+        },
+        /**
          * Given a target dataset, tags the sidewalks with the road network from source dataset which is within the buffer of 5 meters from the sidewalk.
          * @summary Given a target dataset, tags the sidewalks with the road network from source dataset.
          * @param {string} source_dataset_id Dataset from which the road network to be retrieved
@@ -1099,6 +1159,31 @@ var OSWApiFp = function (configuration) {
             });
         },
         /**
+         * Adds the \"incline\" tag to a specified dataset identified by the tdei_dataset_id. It takes the dataset ID as a parameter, processes the dataset to calculate the incline information, and updates the dataset with the incline tag. Returns the job_id for the incline calculation request.
+         * @summary Adds the incline tag to the dataset
+         * @param {string} tdei_dataset_id tdei_dataset_id for a dataset, represented as a uuid
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        datasetTagIncline: function (tdei_dataset_id, options) {
+            return __awaiter(this, void 0, void 0, function () {
+                var localVarAxiosArgs;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, (0, exports.OSWApiAxiosParamCreator)(configuration).datasetTagIncline(tdei_dataset_id, options)];
+                        case 1:
+                            localVarAxiosArgs = _a.sent();
+                            return [2 /*return*/, function (axios, basePath) {
+                                    if (axios === void 0) { axios = axios_1.default; }
+                                    if (basePath === void 0) { basePath = base_1.BASE_PATH; }
+                                    var axiosRequestArgs = __assign(__assign({}, localVarAxiosArgs.options), { url: basePath + localVarAxiosArgs.url });
+                                    return axios.request(axiosRequestArgs);
+                                }];
+                    }
+                });
+            });
+        },
+        /**
          * Given a target dataset, tags the sidewalks with the road network from source dataset which is within the buffer of 5 meters from the sidewalk.
          * @summary Given a target dataset, tags the sidewalks with the road network from source dataset.
          * @param {string} source_dataset_id Dataset from which the road network to be retrieved
@@ -1411,6 +1496,20 @@ var OSWApiFactory = function (configuration, basePath, axios) {
             });
         },
         /**
+         * Adds the \"incline\" tag to a specified dataset identified by the tdei_dataset_id. It takes the dataset ID as a parameter, processes the dataset to calculate the incline information, and updates the dataset with the incline tag. Returns the job_id for the incline calculation request.
+         * @summary Adds the incline tag to the dataset
+         * @param {string} tdei_dataset_id tdei_dataset_id for a dataset, represented as a uuid
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        datasetTagIncline: function (tdei_dataset_id, options) {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    return [2 /*return*/, (0, exports.OSWApiFp)(configuration).datasetTagIncline(tdei_dataset_id, options).then(function (request) { return request(axios, basePath); })];
+                });
+            });
+        },
+        /**
          * Given a target dataset, tags the sidewalks with the road network from source dataset which is within the buffer of 5 meters from the sidewalk.
          * @summary Given a target dataset, tags the sidewalks with the road network from source dataset.
          * @param {string} source_dataset_id Dataset from which the road network to be retrieved
@@ -1605,6 +1704,22 @@ var OSWApi = /** @class */ (function (_super) {
             var _this = this;
             return __generator(this, function (_a) {
                 return [2 /*return*/, (0, exports.OSWApiFp)(this.configuration).datasetBbox(tdei_dataset_id, file_type, bbox, options).then(function (request) { return request(_this.axios, _this.basePath); })];
+            });
+        });
+    };
+    /**
+     * Adds the \"incline\" tag to a specified dataset identified by the tdei_dataset_id. It takes the dataset ID as a parameter, processes the dataset to calculate the incline information, and updates the dataset with the incline tag. Returns the job_id for the incline calculation request.
+     * @summary Adds the incline tag to the dataset
+     * @param {string} tdei_dataset_id tdei_dataset_id for a dataset, represented as a uuid
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof OSWApi
+     */
+    OSWApi.prototype.datasetTagIncline = function (tdei_dataset_id, options) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _this = this;
+            return __generator(this, function (_a) {
+                return [2 /*return*/, (0, exports.OSWApiFp)(this.configuration).datasetTagIncline(tdei_dataset_id, options).then(function (request) { return request(_this.axios, _this.basePath); })];
             });
         });
     };
